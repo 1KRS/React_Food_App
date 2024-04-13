3b0479dea0c5d4e37ca144f7298f1a8616b0eec8
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-04-13 14:53:18 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import fs from 'node:fs/promises';
+import bodyParser from 'body-parser';
 import express from 'express';
 
 // Σύνδεση σε Διακομιστή (Connect to Server)
@@ -13,16 +15,75 @@ const πύλη = 3000;
 // Λειτουργίες (Functions)
 
 // Ενδιάμεσες Λειτουργίες (Middleware)
+διακ.use(bodyParser.json());
+διακ.use(express.static('public'));
+
+διακ.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // GET
+διακ.get('/meals', async (req, res) => {
+  const meals = await fs.readFile('./data/available-meals.json', 'utf8');
+  res.json(JSON.parse(meals));
+});
 
 // POST
+διακ.post('/orders', async (req, res) => {
+  const orderData = req.body.order;
+
+  if (orderData === null || orderData.items === null || orderData.items === []) {
+    return res
+      .status(400)
+      .json({ message: 'Missing data.' });
+  }
+
+  if (
+    orderData.customer.email === null ||
+    !orderData.customer.email.includes('@') ||
+    orderData.customer.name === null ||
+    orderData.customer.name.trim() === '' ||
+    orderData.customer.street === null ||
+    orderData.customer.street.trim() === '' ||
+    orderData.customer['postal-code'] === null ||
+    orderData.customer['postal-code'].trim() === '' ||
+    orderData.customer.city === null ||
+    orderData.customer.city.trim() === ''
+  ) {
+    return res.status(400).json({
+      message:
+        'Missing data: Email, name, street, postal code or city is missing.',
+    });
+  }
+
+  const newOrder = {
+    ...orderData,
+    id: (Math.random() * 1000).toString(),
+  };
+  const orders = await fs.readFile('./data/orders.json', 'utf8');
+  const allOrders = JSON.parse(orders);
+  allOrders.push(newOrder);
+  await fs.writeFile('./data/orders.json', JSON.stringify(allOrders));
+  res.status(201).json({ message: 'Order created!' });
+});
 
 // PUT
 
 // PATCH
 
 // DELETE
+
+// Κώδικας Έπειτα HTTP Αιτημάτων 
+διακ.use((req, res) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  res.status(404).json({ message: 'Not found' });
+});
 
 // Πύλη Διακομιστή (Server Port)
 διακ.listen(πύλη, () => {
