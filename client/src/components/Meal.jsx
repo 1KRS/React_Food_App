614c78f63ea4feaff6cd8e9a3743c45c2808c995
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-04-16 18:08:16 +0200

    Create Meal.jsx

---
+++ client/src/components/Meal.jsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { currencyFormatter } from '../util/formatting';
+import Button from './Button';
+
+const Meal = ({ id, name, image, description, price }) => {
+  return (
+    <MealWrapper>
+      <article>
+        <img src={`http://localhost:8000/${image}`} alt={name} />
+        <div>
+          <h3>{name}</h3>
+          <p className="meal-item-price">{currencyFormatter.format(price)}</p>
+          <p className="meal-item-description">{description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button>Add to Cart</Button>
+        </p>
+      </article>
+    </MealWrapper>
+  );
+};
+
+const MealWrapper = styled.li`
+  background-color: #1d1a16;
+  border-radius: 1rem;
+  overflow: hidden;
+  text-align: center;
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.3);
+
+  article {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  img {
+    width: 100%;
+    height: 20rem;
+    object-fit: cover;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin: 0.75rem 0;
+  }
+
+  .meal-item-description {
+    margin: 1rem;
+  }
+
+  .meal-item-price {
+    display: inline-block;
+    background-color: #312c1d;
+    color: #ffc404;
+    font-size: 0.9rem;
+    font-weight: bold;
+    padding: 0.5rem 2rem;
+    margin: 0;
+    border-radius: 4px;
+  }
+
+  .meal-item-actions {
+    margin-bottom: 1.5rem;
+  }
+`;
+export default Meal;
