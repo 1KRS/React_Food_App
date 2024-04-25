302fa9f2c26780b719a58eacfe60d86bade1d246
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-04-25 23:01:30 +0200

    Create Input.jsx

---
+++ client/src/components/Input.jsx
@@ -0,0 +1,9 @@
+const Input = ({id, label, ...props}) => {
+  return (
+    <p className="control">
+      <label htmlFor={id}>{label}</label>
+      <input id={id} name={id} {...props} required/>
+    </p>
+  );
+};
+export default Input;
